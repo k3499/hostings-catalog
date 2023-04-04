fb713ef45912bbced72e418e680cdd8e0353e686
Kirill Danilychev <k3499@yandex.ru>
2023-04-04 08:36:03 +0300

    single

--- nextjs-coupon/components/singleHead/singlehead.js
+++ nextjs-coupon/components/singleHead/singlehead.js
@@ -1,4 +1,7 @@
 import styles from './singleHead.module.css';
+import utils from '../../styles/utils.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { A11y, Autoplay } from "swiper";
 import Image from 'next/image'
@@ -21,7 +24,7 @@ export default function SingleHead() {
     return (
         <div className={styles.singleHead}>
             <div className={styles.singleHead__swiper}>
-                <Swiper {...swiperParameters} className={styles.swiperSingleHead}>
+                <Swiper {...swiperParameters}>
                     <SwiperSlide>
                     <Image
                         className={styles.swiperSlideImage}
@@ -44,7 +47,13 @@ export default function SingleHead() {
                     </SwiperSlide>
                 </Swiper>
             </div>
-            <div className={styles.singleHead__codeWrap}>2</div>
+            <div className={styles.singleHead__codeWrap}>
+                <div className={styles.singleHead__code}>DATAGAME
+                    <button className={styles.singleHead__promoCopy}><FontAwesomeIcon style={{fontSize:"20px"}} icon={faCopy}></FontAwesomeIcon></button>
+                </div>
+                <p className={styles.singleHead__money}>$0.50</p>
+                <a href="" target='_blank' className={`${styles.singleHead__link} ${utils.button}`}>Получить бонус</a>
+            </div>
         </div>
     );
 }
