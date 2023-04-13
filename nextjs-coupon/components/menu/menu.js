2af69a3ad35faf79006ece3fe08ff10dddab7c6f
Kirill Danilychev <k3499@yandex.ru>
2023-04-14 00:43:54 +0300

    mobilemenu final & sidebar adaptation

---
+++ nextjs-coupon/components/menu/menu.js
@@ -0,0 +1,65 @@
+import styles from "./menu.module.css";
+import Link from "next/link";
+
+export default function Menu() {
+  return (
+    <nav>
+    <ul className={styles.menu__list}>
+      <li className={styles.menu__listItem}>
+        <Link
+          className={`${styles.menu__link}  ${styles.crashLink} ${styles.crashLinkActive}`}
+          href="/crash-csgo-top-saitov"
+        >
+          {" "}
+          Краш
+        </Link>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a className={`${styles.menu__link}  ${styles.jackpot}`} href="#">
+          {" "}
+          Джекпот
+        </a>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a
+          className={`${styles.menu__link}  ${styles.threeColor}`}
+          href="#"
+        >
+          {" "}
+          Три цвета
+        </a>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a className={`${styles.menu__link}  ${styles.fourColor}`} href="#">
+          {" "}
+          Четыре цвета
+        </a>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a className={`${styles.menu__link}  ${styles.box}`} href="#">
+          {" "}
+          Кейсы
+        </a>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a className={`${styles.menu__link}  ${styles.slots}`} href="#">
+          {" "}
+          Слоты
+        </a>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a className={`${styles.menu__link}  ${styles.sportBet}`} href="#">
+          {" "}
+          Ставки на спорт
+        </a>
+      </li>
+      <li className={styles.menu__listItem}>
+        <a className={`${styles.menu__link}  ${styles.sportBet}`} href="#">
+          {" "}
+          Маркеты
+        </a>
+      </li>
+    </ul>
+  </nav>
+  )
+}
