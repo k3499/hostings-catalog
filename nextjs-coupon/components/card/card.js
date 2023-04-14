c0eaed364b334a107760c0bf8657fde3a4227354
k3499 <k3499@yandex.ru>
2023-04-14 18:43:15 +0300

    copy btn

--- nextjs-coupon/components/card/card.js
+++ nextjs-coupon/components/card/card.js
@@ -5,9 +5,30 @@ import { faCircleInfo, faCopy } from "@fortawesome/free-solid-svg-icons"
 import styles from './card.module.css'
 import utils from '../../styles/utils.module.css'
 import logoFail from "/public/images/promokod-csfail.jpg"
+import {useRef} from 'react';
 
 export default function Card({site}) {
     console.log(site);
+    const inputRef = useRef(null);
+
+    const copyToClipboard = (evt) => {
+        console.log(evt.currentTarget.classList);
+
+        const copyButton = evt.currentTarget;
+        let textToCopy = inputRef.current.value;
+        navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            console.log('Текст скопирован в буфер обмена:', textToCopy);
+            copyButton.classList.add('buttonYellow_active');
+            setTimeout(() => {
+                copyButton.classList.remove('buttonYellow_active');
+              }, 500);        
+        })
+        .catch((err) => {
+            console.error('Ошибка при копировании текста в буфер обмена:', err);
+        });
+    };
+
     return (
         <article className={styles.popular__card}>
             <div className={styles.popular__money}>0.50$</div>
@@ -27,8 +48,9 @@ export default function Card({site}) {
             </div>
             <h2 className={styles.popular__cardTitle}><span>Промокод</span> {site.title}</h2>
             <div>
-                <div className={styles.popular__promoCode}>DATAGAME
-                    <button className={styles.popular__promoCopy}><FontAwesomeIcon style={{fontSize:"20px"}} icon={faCopy}></FontAwesomeIcon></button>
+                <div className={styles.popular__promoCodeWrap}>
+                    <input className={styles.popular__promoCode} type="text" value="DATAGAME" readOnly ref={inputRef}/>
+                    <button className={styles.popular__promoCopy}  onClick={copyToClipboard}><FontAwesomeIcon style={{fontSize:"20px"}} icon={faCopy}></FontAwesomeIcon></button>
                 </div>    
             </div>
             <a href="#" target="_blank" className={`${utils.button} ${styles.popular__promoSiteLink}`}>Получить</a>
