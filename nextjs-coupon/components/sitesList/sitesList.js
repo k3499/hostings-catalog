56cf00bad2adb53050c69d0fc411974d45c46ab1
k3499 <k3499@yandex.ru>
2023-04-21 18:48:19 +0300

    category section

---
+++ nextjs-coupon/components/sitesList/sitesList.js
@@ -0,0 +1,15 @@
+import styles from './popular.module.css';
+import Card from '../card/card';
+export default function Popular({ siteList }) {
+    return (
+        <section className={styles.popular}>
+           <h1 className={styles.popular__title}>Популярные сайты</h1> 
+           <div className={styles.popular__hr}></div>
+           <div className={styles.popular__wrapper}>
+           {siteList.map((element) => {
+            return (<Card key={element.id} site={element.attributes}/>);
+           })}
+           </div>
+        </section>
+    );
+}
--- nextjs-coupon/pages/category/[slug].js
+++ nextjs-coupon/pages/category/[slug].js
@@ -1,7 +1,31 @@
+import Head from "next/head"
 import axios from 'axios'
+import Header from "../../components/header/header"
+import Sidebar from "../../components/sidebar/sidebar"
+import Footer from '../../components/footer/footer'
+import homeStyles from "../../styles/Home.module.css"
 
-export default function Category({ slug }){
-    return <h1> { slug }</h1>
+export default function Category({ title, slug, description, page_description, mobileMenu, handleMobileMenu, siteList }){
+    return (
+        <>
+        <Head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name='title' content={title} />
+            <meta name='description' content={description} />
+            <title>{title}</title>
+        </Head>
+        <Header handleMobileMenu={handleMobileMenu} mobileMenu={mobileMenu}/>
+        <div className={homeStyles.wrapper}>
+            <Sidebar />
+            <main className={homeStyles.main}>
+            <sitesList siteList={siteList}/>
+            </main>
+        </div>
+        <Footer description={page_description}/>
+        </>
+    )
 }
 
 
@@ -21,20 +45,19 @@ export async function getStaticProps(ctx) {
         const { slug } = ctx.params;
         const res = await axios.all([
           axios.get('http://127.0.0.1:1337/api/categories/?filters[slug][$eq]=' + slug), 
-          axios.get('http://127.0.0.1:1337/api/sites-lists?categories.slug_eq='+ slug)
+          axios.get('http://127.0.0.1:1337/api/sites-lists?filters[$and][0][categories][slug][$eq]='+ slug)
         ])
         .then(axios.spread((obj1, obj2) => {
-          const homePage = obj1.data.data[0].attributes;
+          const categoryPage = obj1.data.data[0].attributes;
           const siteList = obj2.data.data;
-          console.log(siteList)
-          return {homePage, siteList} 
+          return {categoryPage, siteList} 
           })
         );
         
         
-        const homePage = res.homePage;
+        const categoryPage = res.categoryPage;
         const siteList = res.siteList;
-        return { props: { ...homePage, siteList, slug }};
+        return { props: { ...categoryPage, siteList, slug }};
       } catch (error) {
          return { props: { ...error } };
         }
