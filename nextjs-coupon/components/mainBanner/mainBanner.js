dd5d6cb7434acaea2544e526729cacf01a4d2673
k3499 <k3499@yandex.ru>
2023-05-16 12:00:03 +0300

    fix: mobile adaptive slider

--- nextjs-coupon/components/mainBanner/mainBanner.js
+++ nextjs-coupon/components/mainBanner/mainBanner.js
@@ -1,25 +1,28 @@
 import styles from './mainBanner.module.css';
 import { Russo_One } from 'next/font/google';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { A11y, Autoplay } from "swiper";
+import { A11y, Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/a11y";
 import "swiper/css/autoplay";
+import "swiper/css/pagination";
 
 const russo = Russo_One({ subsets: ['latin', 'cyrillic'], weight: ['400'], fontStyle: ['italic'] });
 
 export default function MainBanner({ mainSlider }) {
     const swiperParameters = {
-        modules: [A11y, Autoplay],
+        modules: [A11y, Autoplay, Pagination],
         loop: true,
         autoplay: { 
             enabled: true,
             delay: 6000, 
             pauseOnMouseEnter: true,
-        }
+        },
+        pagination: { clickable: true },
+        watchSlidesProgress: true,
+        slidesPerGroupAuto: false,
     };
     return (
-        
             <Swiper {...swiperParameters}>
                 {mainSlider.map((element) => {
                     return (
