960cfe8eabf4cb846cd43fd8befcd68ad4a836c3
k3499 <k3499@yandex.ru>
2023-05-23 13:32:14 +0300

    avif webp support fix

--- nextjs-coupon/next.config.js
+++ nextjs-coupon/next.config.js
@@ -2,4 +2,16 @@ module.exports = {
     images: {
       formats: ['image/avif', 'image/webp'],
     },
+    remotePatterns: [
+        {
+            protocol: 'http',
+            hostname: 'localhost',
+            port: '1337'
+        },
+        {
+            protocol: 'https',
+            hostname: 'api.besplatno-skin.com',
+            port: ''
+        },
+      ],
   };
