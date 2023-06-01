f79132ff2680d1e637e1fde560769aff11976828
k3499 <k3499@yandex.ru>
2023-06-01 14:40:21 +0300

    404 page

--- nextjs-coupon/pages/404.js
+++ nextjs-coupon/pages/404.js
@@ -1,58 +1,56 @@
 import Head from "next/head"
 import axios from 'axios'
+import { Russo_One } from 'next/font/google';
 import Header from "../components/header/header"
 import Sidebar from "../components/sidebar/sidebar"
-import MainBanner from "../components/mainBanner/mainBanner"
 import Popular from "../components/popular/popular"
 import homeStyles from "../styles/Home.module.css"
+import notFoundStyles from "../styles/NotFound.module.css"
 
-function Home({ title, description, page_description, siteList, mobileMenu, handleMobileMenu }) {
+const russo = Russo_One({ subsets: ['latin', 'cyrillic'], weight: ['400']});
+
+function Home({siteList, catList }) {
   return (
     <>
       <Head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name='title' content={title} />
-        <meta name='description' content={description} />
-        <title>{title}</title>
+        <meta name='title' content="Страница не найдена | besplatno skin" />
+        <meta name='description' content="К сожалению страница не найдена | besplatno skin" />
+        <title>Страница не найдена | besplatno skin</title>
       </Head>
-      <Header handleMobileMenu={handleMobileMenu} mobileMenu={mobileMenu} />
+      <Header catList={catList} />
       <div className={homeStyles.wrapper}>
-        <Sidebar />
+        <Sidebar catList={catList}/>
         <main className={homeStyles.main}>
-          404
+          <div className={`${notFoundStyles.glitch} ${russo.className} `} data-text="404">404</div>
+          <p className={notFoundStyles.subtitle}>Страница не найдена</p>
+          <h1 className={homeStyles.title}>Возможно вас заинтересует один из проектов:</h1>
+          <div className={homeStyles.hr}></div>
+          <Popular siteList={siteList}/>
         </main>
       </div>
     </>
   )
 }
 
-// export async function getStaticProps() {
-//   try {
-//       const res = await axios.get('http://127.0.0.1:1337/api/homepage');
-      
-//       const homePage = res.data.data.attributes;
-//       return { props:{ ...homePage }};
-//     } catch (error) {
-//       return { error };
-//     }
-// }
+
 export async function getStaticProps(context) {
   try {
     const res = await axios.all([
-        axios.get('http://127.0.0.1:1337/api/homepage'), 
-        axios.get('http://127.0.0.1:1337/api/sites-lists')
+        axios.get('http://127.0.0.1:1337/api/sites-lists?sort[0]=order%3Aasc&populate=logo&populate=slides'),
+        axios.get('http://127.0.0.1:1337/api/categories')
       ])
-      .then(axios.spread((obj1, obj2) => {
-        const homePage = obj1.data.data.attributes;
-        const siteList = obj2.data.data;
-        return {homePage, siteList} 
+      .then(axios.spread(( sites, categoryAll) => {
+        const siteList = sites.data.data;
+        const catList = categoryAll.data.data;
+        return { siteList, catList} 
         })
       );
-      const homePage = res.homePage;
       const siteList = res.siteList;
-      return { props: { ...homePage, siteList }};
+      const catList = res.catList;
+      return { props: { siteList, catList}};
     } catch (error) {
        return { props: { ...error } };
       }
--- nextjs-coupon/pages/_document.js
+++ nextjs-coupon/pages/_document.js
@@ -10,7 +10,9 @@ export default function Document() {
             dangerouslySetInnerHTML={{
               __html: `
                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
       
                 ym(93789546, "init", {
