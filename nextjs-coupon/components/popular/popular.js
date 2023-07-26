f5a24e3b527df87612f346f970dd4b2003c1ef85
k3499 <k3499@yandex.ru>
2023-07-26 12:25:06 +0300

    fornex

--- nextjs-coupon/components/popular/popular.js
+++ nextjs-coupon/components/popular/popular.js
@@ -1,13 +1,13 @@
-import styles from './popular.module.css';
-import Card from '../card/card';
+import styles from "./popular.module.css";
+import Card from "../card/card";
 export default function Popular({ siteList }) {
-    return (
-        <section className={styles.popular}>
-           <div className={styles.wrapper}>
-           {siteList.map((element) => {
-            return (<Card key={element.id} site={element.attributes}/>);
-           })}
-           </div>
-        </section>
-    );
+  return (
+    <section className={styles.popular}>
+      <div className={styles.wrapper}>
+        {siteList.map((element) => {
+          return <Card key={element.id} site={element.attributes} />;
+        })}
+      </div>
+    </section>
+  );
 }
--- nextjs-coupon/pages/index.js
+++ nextjs-coupon/pages/index.js
@@ -1,71 +1,83 @@
-import Head from "next/head"
-import axios from 'axios'
-import { serialize } from "next-mdx-remote/serialize"
-import Header from "../components/header/header"
-import Sidebar from "../components/sidebar/sidebar"
-import MainBanner from "../components/mainBanner/mainBanner"
-import Popular from "../components/popular/popular"
-import homeStyles from "../styles/Home.module.css"
-import Footer from '../components/footer/footer'
+import Head from "next/head";
+import axios from "axios";
+import { serialize } from "next-mdx-remote/serialize";
+import Header from "../components/header/header";
+import Sidebar from "../components/sidebar/sidebar";
+import MainBanner from "../components/mainBanner/mainBanner";
+import Popular from "../components/popular/popular";
+import homeStyles from "../styles/Home.module.css";
+import Footer from "../components/footer/footer";
 
-function Home({ title, description, homePageText, siteList, catList, mainSlider }) {
+function Home({
+  title,
+  description,
+  homePageText,
+  siteList,
+  catList,
+  mainSlider,
+}) {
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="yandex-verification" content="0279ba5f7b8579a8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='yandex-verification' content='0279ba5f7b8579a8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta name='title' content={title} />
         <meta name='description' content={description} />
-        <link rel="canonical" href=""/>
+        <link rel='canonical' href='' />
         <title>{title}</title>
       </Head>
       <Header catList={catList} />
       <div className={homeStyles.wrapper}>
-        <Sidebar catList={catList}/>
+        <Sidebar catList={catList} />
         <main className={homeStyles.main}>
           <div className={homeStyles.mainSliderWrapper}>
-            <MainBanner mainSlider={mainSlider}/>
+            <MainBanner mainSlider={mainSlider} />
           </div>
           <h1 className={homeStyles.title}>Промокоды для хостинга</h1>
           <div className={homeStyles.hr}></div>
-          <Popular siteList={siteList}/>
+          <Popular siteList={siteList} />
         </main>
       </div>
-      <Footer description={homePageText}/>
+      <Footer description={homePageText} />
     </>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
   try {
-    const res = await axios.all([
-        axios.get('http://127.0.0.1:1338/api/homepage'), 
-        axios.get('http://127.0.0.1:1338/api/sites-lists?sort[0]=order%3Aasc&populate=logo&populate=slides'),
-        axios.get('http://127.0.0.1:1338/api/categories'),
-        axios.get('http://127.0.0.1:1338/api/main-banners?populate=image')
+    const res = await axios
+      .all([
+        axios.get("http://127.0.0.1:1338/api/homepage"),
+        axios.get(
+          "http://127.0.0.1:1338/api/sites-lists?sort[0]=order%3Aasc&populate=logo&populate=slides"
+        ),
+        axios.get("http://127.0.0.1:1338/api/categories"),
+        axios.get("http://127.0.0.1:1338/api/main-banners?populate=image"),
       ])
-      .then(axios.spread((home, sites, categoryAll, slider) => {
-        const homePage = home.data.data.attributes;
-        const siteList = sites.data.data;
-        const catList = categoryAll.data.data;
-        const mainSlider = slider.data.data;
-        return {homePage, siteList, catList, mainSlider}
+      .then(
+        axios.spread((home, sites, categoryAll, slider) => {
+          const homePage = home.data.data.attributes;
+          const siteList = sites.data.data;
+          const catList = categoryAll.data.data;
+          const mainSlider = slider.data.data;
+          return { homePage, siteList, catList, mainSlider };
         })
       );
-      const homePage = res.homePage;
-      const siteList = res.siteList;
-      const catList = res.catList;
-      const mainSlider = res.mainSlider;
-      //сериализация markdown
-        const homePageMDX = homePage.page_description;
-        const homePageText = await serialize(homePageMDX)
-      return { props: { ...homePage, siteList, catList, mainSlider, homePageText }};
-    } catch (error) {
-       return { props: { ...error } };
-      }
-
+    const homePage = res.homePage;
+    const siteList = res.siteList;
+    const catList = res.catList;
+    const mainSlider = res.mainSlider;
+    //сериализация markdown
+    const homePageMDX = homePage.page_description;
+    const homePageText = await serialize(homePageMDX);
+    return {
+      props: { ...homePage, siteList, catList, mainSlider, homePageText },
+    };
+  } catch (error) {
+    return { props: { ...error } };
+  }
 }
 
 export default Home;
