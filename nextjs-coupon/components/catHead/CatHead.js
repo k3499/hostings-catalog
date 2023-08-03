efaa845e6ff244d812df14772fdea93d045f0619
k3499 <k3499@yandex.ru>
2023-08-03 18:05:33 +0300

    cat photo link fix

--- nextjs-coupon/components/catHead/CatHead.js
+++ nextjs-coupon/components/catHead/CatHead.js
@@ -1,19 +1,25 @@
-import { Russo_One } from 'next/font/google';
-import styles from './CatHead.module.css'
+import { Russo_One } from "next/font/google";
+import styles from "./CatHead.module.css";
 
-const russo = Russo_One({ subsets: ['latin', 'cyrillic'], weight: ['400'], });
+const russo = Russo_One({ subsets: ["latin", "cyrillic"], weight: ["400"] });
 
-export default function CatHead({title, description, image}) {
-    return (
-        <div className={styles.banner}>
-            <div className={styles.wrapper}>
-                <p className={`${styles.title}  ${russo.className}`}>{ title }</p>
-                <h1 className={`${styles.secondTitle}`}>{ description }</h1>
-            </div>
-            {image.data === null || image.data === undefined
-            ? console.log("no category image")
-            : <div className={styles.bannerImg} style={{ background: `url(https://api.besplatno-skin.com${image.data.attributes.url}) no-repeat`}}></div>
-            }
-        </div>
-    );
+export default function CatHead({ title, description, image }) {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.wrapper}>
+        <p className={`${styles.title}  ${russo.className}`}>{title}</p>
+        <h1 className={`${styles.secondTitle}`}>{description}</h1>
+      </div>
+      {image.data === null || image.data === undefined ? (
+        console.log("no category image")
+      ) : (
+        <div
+          className={styles.bannerImg}
+          style={{
+            background: `url(https://api.hostoncoast.ru${image.data.attributes.url}) no-repeat`,
+          }}
+        ></div>
+      )}
+    </div>
+  );
 }
