92bc6d2553901f3e85f5b80fdd0090fcb00571bf
k3499 <k3499@yandex.ru>
2023-08-08 17:52:31 +0300

    ads banner

--- nextjs-coupon/components/sideBanner/sideBanner.js
+++ nextjs-coupon/components/sideBanner/sideBanner.js
@@ -1,11 +1,14 @@
-import styles from "./sideBanner.module.css"
+import styles from "./sideBanner.module.css";
 
 export default function SideBanner() {
   return (
-    <a href="" className={styles.wrapper} target="_blank">
+    <a
+      href='https://zomro.com/virtual?from=35058'
+      className={styles.wrapper}
+      target='_blank'
+    >
       <span className={styles.label}>Реклама</span>
       <div className={styles.icon}></div>
     </a>
-    //<></>
-  )
+  );
 }
